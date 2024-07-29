a5da0d38ae03d78386ce4eb03406e88bcca11754
Arpita Mishra <15715235-ArpitaMishra1@users.noreply.replit.com>
2024-07-29 20:17:05 +0000

    Final App

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <header className="p-4 ml-6">
-      <h1 className="text-2xl font-bold text-black-600 text-left">XTodo</h1>
+      <h1 className="text-2xl font-bold text-black-600 text-left">ToDoIt</h1>
     </header>
   );
 }
